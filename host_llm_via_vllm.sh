06806983a9d4aee60912656032067a81c0b33923
bijoy <bsbijoy2050@gmail.com>
2024-10-31 16:50:02 -0400

    Modified code

---
+++ host_llm_via_vllm.sh
@@ -0,0 +1,27 @@
+# 1. Create and activate a new conda environment
+conda create -n vllm_env_v3 python=python=3.12
+echo "vllm environment created successfully"
+conda activate vllm_env_v3
+echo "vllm environment activated successfully"
+#conda install conda-forge::nodejs
+#conda install conda-forge::poetry
+
+
+# 2. Install vllm using pip
+pip install vllm
+pip install --upgrade poetry
+pip install --upgrade jinja2
+pip install --upgrade openai
+
+conda deactivate
+echo "vllm environment deactivated successfully"
+conda activate vllm_env_v3
+pip list
+echo "vllm installed successfully"
+# 3. Start the vllm server with Llama 3.1 8B Instruct
+CUDA_VISIBLE_DEVICES=2 vllm serve \
+  /shared_archive/common/models--meta-llama--Llama-3.1-8B-Instruct/snapshots/0e9e39f249a16976918f6564b8830bc894c89659/ \
+  --dtype auto \
+  --api-key token-abc123 \
+  --gpu-memory-utilization 0.9 \
+  --port 8002
