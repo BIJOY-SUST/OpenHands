bf3791d9e5d3c6032f926c3993d92144a3eb42b8
bijoy <bsbijoy2050@gmail.com>
2024-10-31 17:50:00 -0400

    Modified code

--- run_openhands_swe_infer.sh
+++ run_openhands_swe_infer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-conda create -n vllm_env_v3 python=3.12 -c conda-forge
+# conda create -n vllm_env_v3 python=3.12 -c conda-forge
 
-# sh ./evaluation/swe_bench/scripts/run_infer.sh llm.llama-3.1-8b-instruct HEAD CodeActSWEAgent 1 30 1 princeton-nlp/SWE-bench_Lite test
+sh ./evaluation/swe_bench/scripts/run_infer.sh llm.llama-3.1-8b-instruct HEAD CodeActSWEAgent 1 30 1 princeton-nlp/SWE-bench_Lite test
