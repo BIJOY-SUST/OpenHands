f9f96c4837e65b087e6b6ae31139c63da808c4b1
Kaustubh Gayadhankar <kaustubh@context3.cs.stonybrook.edu>
2024-11-21 18:38:56 -0500

    fixing auto checks failed

--- evaluation/swe_bench/run_infer.py
+++ evaluation/swe_bench/run_infer.py
@@ -503,6 +503,9 @@ if __name__ == '__main__':
     llm_config = None
     if args.llm_config:
         llm_config = get_llm_config_arg(args.llm_config)
+        print('Args LLM config path:', args.llm_config)
+        print('LLM Config loaded:', llm_config)
+        print('LLM Config type:', type(llm_config))
         llm_config.log_completions = True
 
     if llm_config is None:
